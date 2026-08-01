ad925e8ffb03b2ebd143af13404a6edac43cd205
Gautam kumar <gautamsiddharth2013@gmail.com>
2026-08-01 12:49:11 +0530

    corrected date format

--- routes/invoiceRoutes.js
+++ routes/invoiceRoutes.js
@@ -1,14 +1,15 @@
 const express = require("express");
-const {createInvoice,getAllInvoices, getInvoiceById, updateInvoice, deleteInvoice} = require("../controller/invoiceController")
+const {createInvoice,getAllInvoices, getInvoiceById, updateInvoice, deleteInvoice, getNextInvoiceNumber} = require("../controller/invoiceController")
 const protect = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-
+router.get("/next-number",  protect, getNextInvoiceNumber);
 router.post("/create", protect, createInvoice);
 router.get("/", protect, getAllInvoices);
 router.get("/:id", protect, getInvoiceById);
 router.put("/:id", protect, updateInvoice);
 router.delete("/:id", protect, deleteInvoice);
+
 
 module.exports = router;
